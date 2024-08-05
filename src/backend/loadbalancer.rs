83a06b2681cc4c83d90452f6f74c13426c5bc693
TorinM <torinmay@gmail.com>
2024-08-05 13:18:05 -0600

    Adding round-robin algorithm to load_balancer

--- src/backend/loadbalancer.rs
+++ src/backend/loadbalancer.rs
@@ -1,3 +1,6 @@
+use std::sync::{Arc, Mutex};
+use std::collections::VecDeque;
+
 use crate::backend::server::Server;
 
 #[derive(Debug, Clone)]
@@ -8,20 +11,27 @@ pub enum Algorithms {
 
 #[derive(Debug, Clone)]
 struct LoadBalancer {
-    pub server_list: Vec<Server>,
+    pub server_list: Arc<Mutex<VecDeque<Server>>>,
     current_server_index: usize,
-    algorithm: Algorithms,
+    algorithm: String
 }
 impl LoadBalancer {
-    pub fn new(server_list: Vec<Server>, algorithm: Algorithms) -> Self {
+    pub fn new(server_list: VecDeque<Server>, algorithm: &str) -> Self {
+        let lower_algorithm = algorithm.to_lowercase();
         Self {
-            server_list,
+            server_list: Arc::new(Mutex::from(server_list)),
             current_server_index: 0,
-            algorithm,
+            algorithm: lower_algorithm
         }
     }
-    pub fn start(&self)
-    {
+    pub async fn handle_request(&self) {
+        match self.algorithm.as_str() {
+            "round-robin" => self.round_robin(),
+            alg => eprintln!("'{}' is not a valid algorithm.", alg) 
+        }
+    }
 
+    fn round_robin(&self) {
+        println!("round-robin");
     }
 }
