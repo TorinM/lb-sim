dc4924cfcccab4e1de8ce6ff19eb94832c4cac6c
TorinM <torinmay@gmail.com>
2024-08-05 11:27:25 -0600

    Moved algorithms to load balancer struct as Enum

--- src/backend/loadbalancer.rs
+++ src/backend/loadbalancer.rs
@@ -1,15 +1,27 @@
 use crate::backend::server::Server;
 
 #[derive(Debug, Clone)]
+pub enum Algorithms {
+    RoundRobin
+}
+
+
+#[derive(Debug, Clone)]
 struct LoadBalancer {
     pub server_list: Vec<Server>,
     current_server_index: usize,
+    algorithm: Algorithms,
 }
 impl LoadBalancer {
-    pub fn new(server_list: Vec<Server>) -> Self {
+    pub fn new(server_list: Vec<Server>, algorithm: Algorithms) -> Self {
         Self {
             server_list,
-            current_server_index: 0
+            current_server_index: 0,
+            algorithm,
         }
+    }
+    pub fn start(&self)
+    {
+
     }
 }
